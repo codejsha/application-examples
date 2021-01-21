522212ce8ea6cfb7410e5f5ecce76b892e214d8c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-21 09:38:42 +0900

    optimize imports

--- kafka/spring-kafka-basic/src/main/java/com/example/basic/BasicApplication.java
+++ kafka/spring-kafka-basic/src/main/java/com/example/basic/BasicApplication.java
@@ -1,8 +1,6 @@
 package com.example.basic;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
