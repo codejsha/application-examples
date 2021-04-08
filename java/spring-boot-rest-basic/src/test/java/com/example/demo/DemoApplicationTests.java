c4c1484bd6238d2c435448637ef83b8379e9b7f7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-08 22:35:56 +0900

    add spring boot rest example

---
+++ java/spring-boot-rest-basic/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.example.demo.controller.DemoController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(DemoController.class)
+class DemoApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloMessage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(MockMvcResultMatchers.content().string("Hello world"));
+    }
+
+    @Test
+    public void testByeMessage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/bye"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(MockMvcResultMatchers.content().string("Good bye"));
+    }
+
+}
